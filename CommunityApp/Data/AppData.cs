09ad2408c02993c93cd98af5b2696a7d7fbc4be3
neSux0 <senxu98@gmail.com>
2026-08-25 10:50:59 -0700

    Added some comments

--- CommunityApp/Data/AppData.cs
+++ CommunityApp/Data/AppData.cs
@@ -6,10 +6,18 @@ namespace CommunityAppMiniProjectWinForms.Data
 {
     internal class AppData
     {
-        //List may be needed for database later.
+        //List may be needed for database later.(uses o(n) search)
         private static List<User> Users { get; } = new();
+        //allows O(1) search for conosle app. Search by id and username.
         private static Dictionary<string, User> usersByUsername = new();
-        private static Dictionary<int, User> userById = new();
+        private static Dictionary<int, User> usersById = new();
+
+        public static void AddUser(User user)
+        {
+            Users.Add(user);
+            usersByUsername.Add(user.Username, user);
+            usersById.Add(user.UserId, user);
+        }
 
 
     }
