291e867ad1af7d3b65f03bf9ed34b0c110e8fc63
neSux0 <senxu98@gmail.com>
2026-08-25 10:33:36 -0700

    added data class containing users as a list + dictionary

---
+++ CommunityApp/Data/AppData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunityAppMiniProjectWinForms.Data
+{
+    internal class AppData
+    {
+        //List may be needed for database later.
+        private static List<User> Users { get; } = new();
+        private static Dictionary<string, User> usersByUsername = new();
+        private static Dictionary<int, User> userById = new();
+
+
+    }
+}
